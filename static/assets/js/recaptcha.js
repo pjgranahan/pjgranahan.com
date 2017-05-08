2fffa49c5fd41a183776860ad662cfcdaa39ae96
Patrick Granahan <pjgranahan@gmail.com>
2017-05-08 14:45:14 -0500

    Working on recaptcha

--- static/assets/js/recaptcha.js
+++ static/assets/js/recaptcha.js
@@ -1,8 +1,24 @@
+var CONTACT_INFO_COOKIE_NAME = "contactInfo";
+
+window.onload = function () {
+    var contactInfo = document.getElementById("contactInfo");
+    contactInfo.onclick = invokeChallenge;
+};
+
 function invokeChallenge() {
     alert("invoking challenge");
 
-    grecaptcha.execute();
+    // Check if contact info exists in cookies
+    var contactInfo = Cookies.getJSON(CONTACT_INFO_COOKIE_NAME);
+    if (contactInfo === undefined) {
+        grecaptcha.render("contactInfo", {sitekey: "{{ .Site.Params.RecaptchaSiteKey }}", size: "compact", callback: "onSuccessfulValidation"});
+    } else {
+        substituteContactInfo(contactInfo)
+    }
+
+    return false;
 }
+
 function onSuccessfulValidation(token) {
     alert(token);
 
@@ -18,19 +34,22 @@ function onSuccessfulValidation(token) {
     http.onreadystatechange = function() {//Call a function when the state changes.
         if(http.readyState == 4 && http.status == 200) {
             alert(http.responseText);
-            // Parse response to JSON
             var contactInfo = JSON.parse(http.responseText);
-
-            // Grab email address 'a' tag and populate its link and text
-            var emailAddressLink = document.getElementById('email_address');
-            emailAddressLink.href = "mailto:" + contactInfo.email_address;
-            emailAddressLink.innerHTML = contactInfo.email_address;
-
-            // Grab phone number 'a' tag and populate its link and text
-            var phoneNumberLink = document.getElementById('phone_number');
-            phoneNumberLink.href = "tel:" + contactInfo.phone_number;
-            phoneNumberLink.innerHTML = contactInfo.phone_number;
+            Cookies.set(CONTACT_INFO_COOKIE_NAME, contactInfo, { expires: 7, path: '', domain: 'www.pjgranahan.com', secure: true });
+            substituteContactInfo(contactInfo);
         }
     };
     http.send(body);
+}
+
+function substituteContactInfo(contactInfo) {
+        // Grab email address 'a' tag and populate its link and text
+    var emailAddressLink = document.getElementById('email_address');
+    emailAddressLink.href = "mailto:" + contactInfo.email_address;
+    emailAddressLink.innerHTML = contactInfo.email_address;
+
+    // Grab phone number 'a' tag and populate its link and text
+    var phoneNumberLink = document.getElementById('phone_number');
+    phoneNumberLink.href = "tel:" + contactInfo.phone_number;
+    phoneNumberLink.innerHTML = contactInfo.phone_number;
 }
